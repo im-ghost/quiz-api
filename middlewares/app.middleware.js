a0b60ca43b708121595876f3312d834950689ace
Ghost <culestfrosh@gmail.com>
2022-11-15 09:21:07 -0800

    app middleware
    removed all the middlewares attached to the app in the app file and added it here

---
+++ middlewares/app.middleware.js
@@ -0,0 +1,34 @@
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+
+module.exports = (app) =>{
+
+    
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // send the error page
+    res.status(err.status || 500);
+    res.send(err);
+  })
+
+}
