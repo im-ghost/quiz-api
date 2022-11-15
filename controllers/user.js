3e98823b0a4155bafb31741d746b779c22ba753f
Ghost <culestfrosh@gmail.com>
2022-11-15 10:30:18 -0800

    user controller

---
+++ controllers/user.js
@@ -0,0 +1,121 @@
+
+const User = require('../models/user.js');
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      quizes: user.quizes,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+}
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = async (req, res) => {
+  
+  const { name, email, password } = req.body
+console.log(req.body)
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name:name,
+    email:email,
+    password: password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      quizes: user.quizes,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+}
+// @desc    Get all users
+// @route   GET /api/user
+const getUsers = async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+}
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+}
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+}
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+for (const attr of user) {
+    user.attr = req.body.attr ? req.body.attr : user.attr
+    
+}
+    const updatedUser = await user.save()
+
+    res.json(updatedUser)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+}
+
+module.exports = {
+  authUser,
+  registerUser,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+}
