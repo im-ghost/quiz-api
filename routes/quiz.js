a0f754adaf61338c609ba436cb775a1dc3740b54
Ghost <culestfrosh@gmail.com>
2022-11-16 10:14:39 -0800

    added the quiz routing

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const {
+    createQuiz,
+    delQuiz,
+    updateQuiz,
+    getQuizById,
+    getUserQuizess
+} = require("../controllers/quiz");
+const {
+  protect,
+  protectMe
+ } = require('../middlewares/auth.middleware');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get("/:id",protect,getUserQuizess)
+router.post("/",createQuiz)
+router.put("/quiz/:id",protect,protectMe,updateQuiz)
+router.delete("/quiz/:id",protect,protectMe ,delQuiz)
+router.get("/quiz/:id",protect,getQuizById)
 module.exports = router;
