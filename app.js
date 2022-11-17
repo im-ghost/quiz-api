2c73292e604f942b81574403755df0b2b0cf03e8
Ghost <culestfrosh@gmail.com>
2022-11-17 01:22:34 -0800

    middlewares
     brought the middlewares back to prevent 404

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require("express")
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const apiRouter = require('./routes/index');
+const cors = require("cors")
+
+const usersRouter = require('./routes/users');
+const quizRouter = require('./routes/quiz');
 
 const app = express();
 const {
   connectDB
 } = require("./config/db")
 connectDB()
-app.use('/api', apiRouter);
-// catch 404 and forward to error handler
 
+app.use(cors({
+  origin:"http://localhost:5173"
+ })) 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use("/api/quiz",quizRouter)
+app.use("/api/users",usersRouter)
+
+app.use(function(req, res, next) {
+  console.log("here")
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // send the error page
+    res.status(err.status || 500);
+    res.send(err);
+  })
 module.exports = app;
