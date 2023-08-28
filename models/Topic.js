634d9d9a6cce6e24002045d261ab070fcf5a6e30
Ghost <96427062+im-ghost@users.noreply.github.com>
2023-08-28 17:48:28 +0100

    Update Topic.js

--- models/Topic.js
+++ models/Topic.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const userSchema = mongoose.Schema(
+  {
+    topic: {
+      type: String,
+      required: true,
+    },
+    others:{
+      type: Object,
+      required: true,
+    }
+  })
+
+const Topic = mongoose.model('Topic', userSchema)
+
+module.exports= Topic
