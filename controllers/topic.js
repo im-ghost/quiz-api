787169842e67763ac876e68d3e0580461b397cae
Ghost <96427062+im-ghost@users.noreply.github.com>
2023-08-28 18:02:49 +0100

    Create topic.js

---
+++ controllers/topic.js
@@ -0,0 +1,23 @@
+const Topic = require("../models/Topic");
+
+const getTopics = async (req,res,next) =>{
+  const topics = await Topic.find()
+  res.status(200).json({topics:topics})
+}
+
+const createTopic = async (req,res) =>{
+  const topic = await Topic.create(req.body);
+  if(topic){
+    res.status(200).json({topic:topic})
+  }else{
+    res.status(400).json({error:'r'})
+  }
+}
+
+const editTopic = async (req,res)=>{
+  const topic = await Topic.findById(req.body.id);
+  if(topic){
+    const newTopic = {...topic,others:req.body.others}
+    res.status(200).json({topic:newTopic})
+  }
+}
