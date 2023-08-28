0eda0dc88406362b4d47972d6d403e4b4695fb18
Ghost <96427062+im-ghost@users.noreply.github.com>
2023-08-28 18:05:42 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const {
+  createTopic,
+  editTopic,
+  getTopics
+} = require("../controllers/topics")
 const router = express.Router();
 /* GET the default API route. */
 router.get('/', (req, res, next) =>{
   res.json('index').status(200);
 });
+router.post('/topic',createTopic)
+router.get('/topic',getTopics)
+router.put('/topic/',editTopic)
 module.exports = router;
